e95302f8fa6783c33313640e4c41f1a8229d65e8
Ishaan-15-Sharma <ishaanavi15sharma@gmail.com>
2024-11-05 17:49:03 +0530

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,7 @@
+import sqlite3 
+def create_db():
+    con=sqlite3.connect(database=r'ims.db') #r here represents path else we have to write 'file name'/folder name'.db and .db is an in built extension
+    cur=con.cursor()
+    cur.execute("CREATE Table IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT,name text,email text,gender text,contact text,dob text,doj text,pass text,utype text,address text,salary text)")
+    con.commit()
+create_db()
