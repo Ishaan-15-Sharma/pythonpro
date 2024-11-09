929f6e2b79587dee1d7cc66885006923111e9334
Ishaan-15-Sharma <ishaanavi15sharma@gmail.com>
2024-11-09 15:00:45 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -4,4 +4,9 @@ def create_db():
     cur=con.cursor()
     cur.execute("CREATE Table IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT,name text,email text,gender text,contact text,dob text,doj text,pass text,utype text,address text,salary text)")
     con.commit()
+
+    cur.execute("CREATE Table IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTOINCREMENT,name text,contact text,desc text)")
+    con.commit()
+
+
 create_db()
